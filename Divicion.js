196195ae4e20990d5218c545e11e0f5c68a836b1
Serrano98 <maos.isc@gmail.com>
2021-10-25 17:21:31 -0500

    1 file modified, 1 added

---
+++ Divicion.js
@@ -0,0 +1,7 @@
+var colors = require('colors');
+
+function divicion(num1, num2) {
+  console.log('El resultado de la divicion es:'.red);
+  console.log(num1 + num2);
+}
+module.exports = divicion;
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // run `node index.js` in the terminal
 
 var sumar = require('./Sumar');
+var sumar = require('./Divicion');
+
 sumar(2, 4);
+divicion(10, 5);
