b19e68d6859571d47598171c1fa6357a26fb756f
Serrano98 <maos.isc@gmail.com>
2021-10-25 17:26:02 -0500

    1 file modified

--- Sumar.js
+++ Sumar.js
@@ -5,6 +5,5 @@ var misCpu = os.cpus();
 function sumar(num1, num2) {
   console.log('El resultado de la suma es:'.blue);
   console.log(num1 + num2);
-  console.log(misCpu);
 }
 module.exports = sumar;
